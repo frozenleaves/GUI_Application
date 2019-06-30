0f60450e25e4cf8526a54b0aeaf92cb2e21b27f5
frozenleaves <914814442@qq.com>
2019-06-30 15:33:44 +0800

    表单布局

--- designer/表单布局_运行.py
+++ designer/表单布局_运行.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 
-
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import 混合布局 as hb
+import 表单布局 as bb
 
 scale = 0.6
 size = (int(1920 * scale), int(1080 * scale))
 
 app = QApplication(sys.argv)
 mainWindow = QMainWindow()
-ui = hb.Ui_MainWindow()
+ui = bb.Ui_MainWindow()
 
 ui.setupUi(mainWindow)
 mainWindow.resize(*size)
-mainWindow.setWindowTitle('混合布局')
+mainWindow.setWindowTitle('表单布局')
 
 mainWindow.show()
 sys.exit(app.exec_())
