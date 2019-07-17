b00e64453cb5f53a2b1cdfc2185c7c5a86fa5e05
frozenleaves <914814442@qq.com>
2019-07-17 16:32:42 +0800

    从代码设计窗口

---
+++ FromCode/first_window.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QIcon
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        # 设置窗口标题
+        self.setWindowTitle('第一个主窗口')
+
+        # 设置窗口大小
+        self.resize(800, 600)
+
+        # 设置应用图标
+
+        # 获取状态栏
+        self.status = self.statusBar()
+
+        # 设置状态栏信息，显示5秒后关闭
+        self.status.showMessage('消息显示五秒，之后消失', 5000)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    app.setWindowIcon(QIcon('./images/枫叶.png'))
+
+    window = MainWindow()
+
+    window.show()
+
+    sys.exit(app.exec_())
