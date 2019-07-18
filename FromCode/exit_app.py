b1a8fb8555070310fac5ac68a7889bd908c1c8d1
frozenleaves <914814442@qq.com>
2019-07-18 19:06:30 +0800

    添加注释

--- FromCode/exit_app.py
+++ FromCode/exit_app.py
@@ -13,14 +13,25 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('退出应用程序')
         self.resize(800, 600)
 
+        # 添加一个按钮
         self.button1 = QPushButton('quit')
-        # self.button1.setGeometry(QtCore.QRect(610, 470, 93, 28))
+
+        # 按钮关联自定义的槽
         self.button1.clicked.connect(self.onClickButton)
 
+        # 添加水平布局
         layout = QHBoxLayout()
+
+        # 放置按钮组件
         layout.addWidget(self.button1)
+
+        # 添加组件主框架
         mainFrame = QWidget()
+
+        # 将布局设置在主框架上
         mainFrame.setLayout(layout)
+
+        # 将注框架放置在屏幕上
         self.setCentralWidget(mainFrame)
 
     def onClickButton(self):
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GUI_Application
 基于PyQt5的桌面应用
+
+#### .ui 为qt designer 生成的UI文件，相应的.py文件为pyUIC自动转化而来
+
+
